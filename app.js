12bbed82b1657cc4e48e762459a1532f6bdca07c
Isaac Newton Tingo <newtone480@gmail.com>
2022-04-18 14:10:08 +0300

    Changed cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require("express-async-errors");
+require("dotenv").config();
 require("./db");
 const express = require("express");
-require("dotenv").config();
 //const morgan = require("morgan");
 const postRouter = require("./routers/post");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: true }));
 app.use(express.json());
 //app.use(morgan("dev"));
 app.use("/api/post", postRouter);
